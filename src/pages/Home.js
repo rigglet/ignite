9b27769e1474cd23273ac9fa228631d5ea650ef1
rigglet <39748939+rigglet@users.noreply.github.com>
2021-01-02 16:19:35 +0000

    7. Game styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,52 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
+
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  //fetch games
+  //fetch games into state
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  //get data from state
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
