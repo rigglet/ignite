16295adfd08f80ebeffaf35cc3f950a2a4aacd24
rigglet <39748939+rigglet@users.noreply.github.com>
2021-01-02 13:14:12 +0000

    5. Get game data

--- src/index.js
+++ src/index.js
@@ -9,12 +9,9 @@ import rootReducer from "./reducers";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-const composeEnhancer =
-  (window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()) ||
-  compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer);
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
